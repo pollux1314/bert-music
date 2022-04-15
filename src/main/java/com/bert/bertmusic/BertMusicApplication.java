6c4e1f7702814e599f557fc8ca6c05d7e75f1bc6
bert.shen <184387165@qq.com>
2022-04-15 20:15:17 +0800

    first commit

---
+++ src/main/java/com/bert/bertmusic/BertMusicApplication.java
@@ -0,0 +1,13 @@
+package com.bert.bertmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BertMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BertMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bert/bertmusic/BertMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bert.bertmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BertMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
