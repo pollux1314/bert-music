1fde67da7fe3604185881f0cc0304f9860ff2507
沈佳伟 <184387165@qq.com>
2022-04-16 16:55:04 +0800

    跨域修改

--- src/main/java/com/bert/bertmusic/controller/DefaultController.java
+++ src/main/java/com/bert/bertmusic/controller/DefaultController.java
@@ -1,16 +1,18 @@
 package com.bert.bertmusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
 
 
     @GetMapping
     public String sayHello() {
-        return "Hello World!";
+        return "欢迎使用波特音乐盒!";
     }
 }
